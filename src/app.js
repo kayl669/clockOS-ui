b6721b745f457033f8a6193b23df2ad866463a5c
Stefano Gerli <sgerli@igerli.com>
2017-01-07 17:25:28 -0600

    Updated Styles and fonts

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@
 // Declare app level module which depends on views, and components
 angular.module('clockOS', [
   'ngRoute',
+  'routeStyles',
   'clockOS.home',
   'clockOS.view1',
   'clockOS.view2',
