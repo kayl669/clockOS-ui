1adcfcca48234e542cb1cbfa78ad7654213a3bcc
Stefano Gerli <sgerli@igerli.com>
2017-01-31 12:01:50 -0600

    Add transitions

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { fadeInZoom } from '../animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [fadeInZoom]
 })
 export class HomeComponent implements OnInit {
+
+  @HostBinding('@routeAnimation') routeAnimation = true;
+  @HostBinding('style.display')   display = 'block';
+  @HostBinding('style.position')  position = 'relative';
 
   constructor() { }
 
