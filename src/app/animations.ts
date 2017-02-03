162b0a0debf44991002ba4792d2c46d79671108c
Stefano Gerli <sgerli@igerli.com>
2017-02-03 15:44:11 -0600

    Added nice animations and more styling

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,27 +1,19 @@
 import { animate, AnimationEntryMetadata, state, style, transition, trigger } from '@angular/core';
 
 // Component transition animations
-export const slideInDownAnimation: AnimationEntryMetadata =
+export const rightInAnimation: AnimationEntryMetadata =
   trigger('routeAnimation', [
     state('*',
       style({
-        opacity: 1,
         transform: 'translateX(0)'
       })
     ),
     transition(':enter', [
       style({
-        opacity: 0,
-        transform: 'translateX(-100%)'
+        transform: 'translateX(100%)'
       }),
-      animate('0.2s ease-in')
+      animate('0.08s ease-in')
     ]),
-    transition(':leave', [
-      animate('0.5s ease-out', style({
-        opacity: 0,
-        transform: 'translateY(100%)'
-      }))
-    ])
   ]);
 
   export const fadeInZoom: AnimationEntryMetadata =
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 import { MdList } from '@angular/material'
+
+import { rightInAnimation } from '../animations';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
+  animations: [ rightInAnimation ]
 })
 export class MenuComponent implements OnInit {
+
+  @HostBinding('@routeAnimation') routeAnimation = true;
+  @HostBinding('style.display')   display = 'block';
+  @HostBinding('style.position')  position = 'relative';
 
   constructor() { }
 
